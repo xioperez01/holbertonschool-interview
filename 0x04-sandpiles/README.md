9d2f85978c780cc7f8eebc277c8b375f78a1c620
xioperez01 <angie.xioperez@gmail.com>
2021-05-20 18:31:01 -0500

    initial comit

---
+++ 0x04-sandpiles/README.md
@@ -0,0 +1,3 @@
+# Sandpiles
+
+Algorithms
---
+++ 0x04-sandpiles/sandpiles.h
@@ -0,0 +1,10 @@
+#ifndef SANDPILES.H
+#define SANDPILES.H
+
+/* PROTOTYPES */
+
+static void print_grid_sum(int grid1[3][3], int grid2[3][3]);
+static void print_grid(int grid[3][3]);
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+
+#endif
