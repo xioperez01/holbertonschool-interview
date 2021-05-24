94570e9c4fc5a33b78a247243fdf87e46c2253b5
xioperez01 <angie.xioperez@gmail.com>
2021-05-24 16:24:43 -0500

    Done

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -1,10 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
 /**
- * is_palindrome - Function in C that checks if a singly linked list is a palindrome.
+ * is_palindrome - Function that checks if a singly linked list is a palindrome
  * * An empty list is considered a palindrome
  * @head: Pointer to singly linked list
  * Return: 0 if it is not a palindrome, 1 if it is a palindrome
  */
 
-int is_palindrome(listint_t **head) {
+int is_palindrome(listint_t **head)
+{
+	listint_t *tmp = *head;
+	int *list, counter = 0;
+
+	list = malloc(sizeof(int));
+
+	if (*head == NULL)
+		return (1);
+
+	while (tmp)
+	{
+		list[counter] = tmp->n;
+		tmp = tmp->next;
+		counter++;
+	}
+
+	tmp = *head;
+	counter = counter - 1;
+
+	while (tmp)
+	{
+		if (tmp->n == list[counter])
+		{
+			tmp = tmp->next;
+			counter--;
+		}
+		else
+			return (0);
+	}
+
+	return (1);
 
 }
