cc1c8faf5b452511c7fffb31c8a98846a06b12f3
xioperez01 <angie.xioperez@gmail.com>
2021-05-26 12:21:49 -0500

    Efficiency: With recursion

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -3,6 +3,32 @@
 #include "lists.h"
 
 /**
+ * with_recursion - Auxiliar recursion for is_palindrome
+ * @prev: Pointer to head
+ * @next: Head of singly linked list
+ *
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+
+int with_recursion(listint_t **prev, listint_t *next)
+{
+	int ret;
+
+	if (next == NULL)
+		return (1);
+
+	ret = with_recursion(prev, next->next);
+	if (ret == 0)
+		return (0);
+
+	ret = (next->n == (*prev)->n);
+
+	*prev = (*prev)->next;
+
+	return (ret);
+}
+
+/**
  * is_palindrome - Function that checks if a singly linked list is a palindrome
  * * An empty list is considered a palindrome
  * @head: Pointer to singly linked list
@@ -11,30 +37,7 @@
 
 int is_palindrome(listint_t **head)
 {
-	listint_t *tmp = *head;
-	int *list, counter = 0, i;
-
-	list = malloc(sizeof(int));
-
-	if (*head == NULL)
-		return (1);
-
-	while (tmp)
-	{
-		list[counter] = tmp->n;
-		tmp = tmp->next;
-		counter++;
-	}
-
-	counter = counter - 1;
-
-	for (i = 0; i <= (counter / 2); i++)
-	{
-		if (list[counter] == list[i])
-			counter--;
-		else
-			return (0);
-	}
-
-	return (1);
+	if (!head)
+		return (0);
+	return (with_recursion(head, *head));
 }
--- 0x05-linked_list_palindrome/lists.h
+++ 0x05-linked_list_palindrome/lists.h
@@ -19,6 +19,7 @@ size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
+int with_recursion(listint_t **prev, listint_t *next);
 int is_palindrome(listint_t **head);
 
 #endif /* LISTS_H */
