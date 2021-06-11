d52210f54305d776a80f888caa0171593cd3e33a
xioperez01 <angie.xioperez@gmail.com>
2021-06-10 19:23:13 -0500

    Initial code

---
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+* check_cycle - Function that checks if a singly linked list has a cycle in it
+* @list: Pointer to head of singly linked list
+* Return: 0 if there is no cycle, 1 if there is a cycle
+*/
+
+int check_cycle(listint_t *list)
+{
+
+	listint_t *tmp = list;
+
+	if (list == NULL)
+		return (1);
+
+	while (tmp != NULL)
+	{
+		if (tmp->next == list)
+			return (1);
+		tmp = tmp->next;
+	}
+
+	return (0);
+}
