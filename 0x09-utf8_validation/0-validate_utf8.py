df4153ebae74bca4360bebb90e941236abb746ec
xioperez01 <angie.xioperez@gmail.com>
2021-06-24 15:41:53 -0500

    Done x 2!

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -11,27 +11,23 @@ def validUTF8(data):
     Return: True if data is a valid UTF-8 encoding, else return False
     '''
 
-    i = 0
-    while i < len(data):
-        n = data[i] & (2 ** 7)
-        n >>= (7)
-        # single byte char
-        if n == 0:
-            i += 1
-            continue
+    index = 0
 
-        # validate multi-byte char
-        if n >> 5 == 0b110 and i < len(data) - 1:
-            if data[i + 1] >> 6 == 2:
-                i += 2
+    for num in data:
+        byte = format(num, '#010b')[-8:]
+        if index == 0:
+            for b in byte:
+                if b == '0':
+                    break
+                index += 1
+            if index == 0:
                 continue
-        if n >> 4 == 0b1110 and i < len(data) - 2:
-            if data[i + 1] >> 6 == data[i + 2] >> 6 == 2:
-                i += 3
-                continue
-        if n >> 3 == 0b11110 and i < len(data) - 3:
-            if data[i + 1] >> 6 == data[i + 2] >> 6 == data[i + 3] >> 6 == 2:
-                i += 4
-                continue
-        return False
-    return True
+            if index == 1 or index > 4:
+                return False
+        else:
+            if not (byte[0] == '1' and byte[1] == '0'):
+                return False
+        index -= 1
+
+    return index == 0
+
