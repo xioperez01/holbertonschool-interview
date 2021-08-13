81451717c8cf566a1d8d325cfb91e02751cb690f
xioperez01 <angie.xioperez@gmail.com>
2021-08-12 20:15:26 -0500

    Fixing import request

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-import request from "request";
+const query = require("request");
 const filmID = process.argv[2];
 
 const names = [];
 let char = [];
 
-request(
+query(
   `https://swapi-api.hbtn.io/api/films/${filmID}`,
   function (error, response, body) {
     if (error !== null) {
@@ -14,7 +14,7 @@ request(
     const data = JSON.parse(body);
     char = data.characters;
     char.forEach((character, idx) => {
-      request(`${character}`, function (error, response, body) {
+      query(`${character}`, function (error, response, body) {
         if (error !== null) {
           console.error("error:", error);
         }
