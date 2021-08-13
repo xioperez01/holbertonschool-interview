02217da72c32c72d4e7472af3734f8e707da2230
xioperez01 <angie.xioperez@gmail.com>
2021-08-12 20:20:28 -0500

    Fixing semistandard style

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-const query = require("request");
+
+const query = require('request');
 const filmID = process.argv[2];
 
 const names = [];
@@ -9,14 +10,14 @@ query(
   `https://swapi-api.hbtn.io/api/films/${filmID}`,
   function (error, response, body) {
     if (error !== null) {
-      console.error("error:", error);
+      console.error('error:', error);
     }
     const data = JSON.parse(body);
     char = data.characters;
     char.forEach((character, idx) => {
       query(`${character}`, function (error, response, body) {
         if (error !== null) {
-          console.error("error:", error);
+          console.error('error:', error);
         }
         const data = JSON.parse(body);
         names[idx] = data.name;
