7a57ecc3c0f8ab134541a06958ae84ae77c91c38
xioperez01 <angie.xioperez@gmail.com>
2021-08-29 18:43:19 -0500

    Initial code

--- 0x11-heap_sort/0-heap_sort.c
+++ 0x11-heap_sort/0-heap_sort.c
@@ -1,73 +1,92 @@
 #include "sort.h"
-/**
- * swap - swaps two positions
- * @array1: array1
- * @array2: array2
- * Return: void
- */
-void swap(int *array1, int *array2)
-{
-	int temp;
-
-	temp = *array1;
-	*array1 = *array2;
-	*array2 = temp;
-}
-/**
- * heap - makes array into a heap
- * @array: ptr to int array
- * @size: size of array
- * @i: max point
- * @length: size of the array in heap_sort fn
- * Return: is a void
- */
-void heap(int *array, int size, int i, int length)
-{
-	int max = i;
-	int left = 2 * i + 1;
-	int right = 2 * i + 2;
-
-	if (left < size && array[max] < array[left])
-	{
-		max = left;
-	}
-
-	if (right < size && array[max] < array[right])
-	{
-		max = right;
-	}
-
-	if (max != i)
-	{
-		swap(&array[i], &array[max]);
-		print_array(array, length);
-		heap(array, size, max, length);
-	}
-}
 
 /**
- * heap_sort - sort and array (heap method)
- * @array: array to sort
- * @size: size of array
- * Return: is a void
+ * heap_sort - sift-down heap sort algorithm
+ *
+ * @array: array to be sorted
+ * @size: size of array to be sorted
+ *
  */
 void heap_sort(int *array, size_t size)
 {
-	int i;
+	size_t last;
 
-	if (!size || !array)
+	if (!array || size < 2)
 		return;
-	for (i = size / 2 - 1; i >= 0; i--)
+
+	sort(array, size);
+
+	for (last = size; last > 1;)
 	{
-		heap(array, size, i, size);
-	}
-	for (i = size - 1; i > 0; i--)
-	{
-		if (array[0] >= array[i])
-		{
-			swap(&array[0], &array[i]);
-			print_array(array, size);
-		}
-		heap(array, i, 0, size);
+		swap(array, size, &array[0], &array[last - 1]);
+		sift_down(array, 0, --last, size);
 	}
 }
+
+/**
+ * swap - to swap two integers
+ * @array: array to be sorted
+ * @size: the size of the array to be sorted
+ * @first: pointer of the first integer
+ * @second: pointer of the second integer
+ *
+ */
+void swap(int *array, size_t size, int *first, int *second)
+{
+	int temp;
+
+	if (*first != *second)
+	{
+		temp = *first;
+		*first = *second;
+		*second = temp;
+	}
+
+	print_array((const int *)array, size);
+}
+
+/**
+ * sift_down - heap sort shift down
+ *
+ * @array: the array to be sorted
+ * @first: first element of the array
+ * @last: last element of the array
+ * @size: size of array
+ *
+ */
+void sift_down(int *array, size_t first, size_t last, size_t size)
+{
+	size_t root = first, swp;
+
+	swp = root;
+	for (; left_ch(root) < last; root = swp)
+	{
+		swp = root;
+
+		if (array[left_ch(root)] > array[root])
+			swp = left_ch(root);
+
+		if (right_ch(root) < last &&
+		    array[right_ch(root)] > array[swp])
+			swp = right_ch(root);
+
+		if (swp == root)
+			break;
+
+		swap(array, size, &array[root], &array[swp]);
+	}
+}
+
+/**
+ * sort - heap sort in place
+ *
+ * @array: the array to be sorted
+ * @size: size of the array
+ *
+ */
+void sort(int *array, size_t size)
+{
+	ssize_t first;
+
+	for (first = parent(size - 1); first >= 0; first--)
+		sift_down(array, first, size, size);
