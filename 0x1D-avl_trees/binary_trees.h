a83666dddcc3f9600394df17a3dc96bfc6f0db7d
xioperez01 <angie.xioperez@gmail.com>
2022-03-03 13:27:32 -0500

    Task

--- 0x1D-avl_trees/binary_trees.h
+++ 0x1D-avl_trees/binary_trees.h
@@ -1,5 +1,7 @@
-#ifndef BINARY_TREE_H
-#define BINARY_TREE_H
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
+
+#include <stddef.h>
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -10,13 +12,17 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s avl_t;
 
-#endif /* BINARY_TREE_H */
+int binary_tree_is_avl(const binary_tree_t *tree);
+void binary_tree_print(const binary_tree_t *tree);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+#endif /* _BINARY_TREES_H_ */
